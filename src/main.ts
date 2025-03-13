e24dbb03142b5143150ceebbea3a46e02dc61bcc
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-13 14:00:22 +0300

    Исправлено подключение

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { TodoModule } from './todo.module';
 async function bootstrap() {
   const app = await NestFactory.create(TodoModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000',' http://192.168.0.107:3000']
+    origin: ['http://localhost:3001', 'http://localhost:3000','http://192.168.0.107:3000']
   });
   await app.listen(process.env.PORT ?? 3001);
 }
