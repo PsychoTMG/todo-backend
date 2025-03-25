0fa2ec674fb4981fd4aee40eea9e636d561ed200
PsychoTMG <psychotmg@xiaomi-25-pro-ulta-250tb-5000mpx.local>
2025-03-25 17:55:27 +0300

    деплой

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://192.168.0.107:3000']
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
